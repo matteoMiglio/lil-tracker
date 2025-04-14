b5d43982377990575cedd6326fba0c69cae04488
Matteo Miglioli <matteo.miglioli95@gmail.com>
2025-04-14 16:28:58 +0200

    feat: update entrypoint script to include database seeding step

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Run Prisma Generate (for making sure schema is updated)
 bunx prisma generate
 
+# Run Prisma Migrate to apply any pending migrations
 bunx prisma migrate deploy
 
+bunx prisma db seed
+
+# Start the app
 exec "$@"
